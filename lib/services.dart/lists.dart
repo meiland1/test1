66f0e033e3cbe030cd5f2014ed6f338a00af1702
Mason Eiland <65780345+meiland1@users.noreply.github.com>
2023-03-07 13:06:01 -0700

    first commit

---
+++ lib/services.dart/lists.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+List habitList = [
+  [
+    false,
+    "Drink Water",
+    "Drink at least 8 cups of water",
+    Icon(Icons.local_drink)
+  ],
+  [
+    false,
+    "Study",
+    "keep up with class assignments",
+    Icon(Icons.book),
+  ],
+  [
+    false,
+    "Code",
+    "work on coding project",
+    Icon(Icons.computer),
+  ],
+];
